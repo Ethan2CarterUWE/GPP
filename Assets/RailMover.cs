35cf6595f436041d07d2f29a91ae7397f9438423
Ethan <Ethan2.Carter@live.uwe.ac.uk>
2020-08-01 22:15:33 +0100

    spline

---
+++ Assets/RailMover.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RailMover : MonoBehaviour
+{
+    public Rail rail;
+    public Transform lookAt;
+    public bool smoothMove = true;
+
+    private Transform thisTransform;
+    private Vector3 lastPosition;
+
+    private void Start()
+    {
+        thisTransform = transform;
+        lastPosition = thisTransform.position;
+    }
+
+    //FixedUpdate over Update so that the camera does not wobble
+    private void FixedUpdate()
+    {
+        //smooths the transitions of the camera between points
+        if (smoothMove)
+        {
+            thisTransform.position = lastPosition;
+            lastPosition = Vector3.Lerp(lastPosition, rail.ProjectPositionOnRail(lookAt.position), Time.deltaTime);
+        }
+        else
+        {
+            thisTransform.position = rail.ProjectPositionOnRail(lookAt.position);
+        }
+
+        thisTransform.LookAt(lookAt.position);
+    }
+
+}
