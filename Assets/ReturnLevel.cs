b4561a1bd2d23dda95801d0cf68c9e6758dda1c3
Ethan <Ethan2.Carter@live.uwe.ac.uk>
2020-08-04 23:24:51 +0100

    rtyyt

---
+++ Assets/ReturnLevel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnLevel : MonoBehaviour
+{
+
+    private int previousSceneLoad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        previousSceneLoad = SceneManager.GetActiveScene().buildIndex - 1;
+
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(previousSceneLoad);
+    }
+}
